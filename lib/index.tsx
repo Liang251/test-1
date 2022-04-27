59e3be6aadc316cd021e1c288c858ea020db6355
梁251 <615803223@qq.com>
2022-04-27 20:22:43 +0800

    安装了react和reactdom

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,11 @@
 
-console.log('hisossss')
+/*console.log('hisossss')
 var a = document.createElement('div')
 a.textContent="这是我从index.tsx创建的div元素"
-document.body.appendChild(a)
+document.body.appendChild(a)*/
+
+import React from "react";
+import react from "react";
+import ReactDOM from 'react-dom';
+
+console.log(React)
